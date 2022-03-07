66fffcce2e3f419196810fc1d9fd5b2be2af7881
ghkkk135 <s10371302@gmail.com>
2022-03-07 11:49:23 +0800

    加入專案檔案。

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double meal_cost = 100;
+            int tip_percent = 15;
+            int tax_percent = 8;
+            Console.WriteLine("用餐費用：" + (meal_cost + tip_percent + tax_percent));
+        }
+    }
+}
